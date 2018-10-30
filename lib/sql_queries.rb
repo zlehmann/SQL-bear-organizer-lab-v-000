b9a009cdb60731c25975e5877a9aeafb827c8c39
Zachary Lehmann <zlehmann@gmail.com>
2018-10-30 11:12:37 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,7 +19,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color, ColorCount = (SELECT COUNT(color) FROM bears) FROM bears;"
+  "SELECT color, MAX(SELECT COUNT(color) FROM bears) FROM bears;"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
