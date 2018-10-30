14c970f855d51fb895faf1e46ae37f3c235763aa
Zachary Lehmann <zlehmann@gmail.com>
2018-10-30 02:04:32 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -3,7 +3,7 @@ def selects_all_female_bears_return_name_and_age
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT name FROM bears ASC"
+  "SELECT name FROM bears ASC;"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
