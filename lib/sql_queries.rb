5dfefbf6247c2ee399ff1bdc76a60bd0ab97c5c6
Zachary Lehmann <zlehmann@gmail.com>
2018-10-30 02:16:55 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -1,5 +1,5 @@
 def selects_all_female_bears_return_name_and_age
-  "SELECT name, age FROM bears WHERE gender = 'female';"
+  "SELECT name, age FROM bears WHERE gender = female;"
 end
 
 def selects_all_bears_names_and_orders_in_alphabetical_order
@@ -23,7 +23,7 @@ def selects_most_prominent_color_and_returns_with_count
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "SELECT COUNT(WHERE temperaments = "goofy") FROM bears;"
+  "SELECT COUNT(WHERE temperaments = goofy) FROM bears;"
 end
 
 def selects_bear_that_killed_Tim
