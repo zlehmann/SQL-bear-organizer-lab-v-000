5c0f84d3c414afeaf245f6163fbe692fc8ef0b5b
Zachary Lehmann <zlehmann@gmail.com>
2018-10-30 01:54:04 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,9 +1,9 @@
-"CREATE TABLE bears (
-  id INTEGER;
-  name TEXT;
-  age INTEGER;
-  gender TEXT;
-  color TEXT;
-  temperament TEXT;
-  alive INTEGER;
-);"
+CREATE TABLE bears (
+  id INTEGER,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive INTEGER
+);
