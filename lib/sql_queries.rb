bd005b9e4d5b23c5908d9b3ada454b921b342a57
Zachary Lehmann <zlehmann@gmail.com>
2018-10-30 02:40:45 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,8 +11,7 @@ def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT MAX(age) FROM bears AS oldest;"
-  "SELECT name, age FROM oldest;"
+  "SELECT name, age, MAX(age) FROM bears;
 end
 
 def select_youngest_bear_and_returns_name_and_age
